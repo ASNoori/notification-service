858dd63e395720be5a7b46dbeb87073813a564ff
ASNoori <nooriameer12@gmail.com>
2024-08-16 07:28:25 +0530

    Defined the email options, including sender, recipient, subject, and body text

--- email-utils.js
+++ email-utils.js
@@ -1,14 +1,20 @@
-require('dotenv').config(); 
-import { createTransport } from 'nodemailer';
+require("dotenv").config();
+import { createTransport } from "nodemailer";
 
 async function sendEmail({ to, subject, text }) {
-    const transporter = createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,       
-          pass: process.env.EMAIL_PASS, 
-        }
-      });
-    }
+  const transporter = createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+}
 
 export default { sendEmail };
